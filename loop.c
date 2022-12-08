e7dd284b173bc8c2205b8181851161b90285d614
Raquel Bezerra <raquelmcbezerra@gmail.com>
2022-12-07 16:53:18 -0800

    Write a UNIX command line interpreter.

--- loop.c
+++ loop.c
@@ -1,6 +1,6 @@
 #include "shellheader.h"
 /**
- *
+ * shell_loop - print a prompt
  *
  */
 void shell_loop(void)
@@ -8,17 +8,17 @@ void shell_loop(void)
 	char *line;
 	char **tokens;
 
-	    while(true)
-	    {
-            printf("$ ");
+	while (true)
+	{
+		printf("$ ");
+		line = shell_read_line();
+		tokens = shell_split_line(line);
 
-	    line = shell_read_line();
-	    tokens = shell_split_line(line);
-
-	            if (tokens[0] != NULL) {
-	                shell_exec(tokens);
-		        }
-	            free(tokens);
-            free(line);
-        }
+	if (tokens[0] != NULL)
+	{
+		shell_exec(tokens);
+	}
+		free(tokens);
+		free(line);
+	}
 }
--- read.c
+++ read.c
@@ -1,14 +1,14 @@
 #include "shellheader.h"
 /**
- *
- *
+ * shell_read_line - read commands from a text file
+ * Return: line
  */
 char *shell_read_line()
 {
 	    char *line = NULL;
-	        size_t buflen = 0;
+	    size_t buflen = 0;
 
-	         getline(&line, &buflen, stdin);
+	    getline(&line, &buflen, stdin);
 
-	        return (line);
+	return (line);
 }
