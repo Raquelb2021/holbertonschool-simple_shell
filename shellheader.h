6028e1503080bdfaf4828aca1f9ee9029aa68bb7
NikkiMerena <alderman.nikki1022@gmail.com>
2022-12-14 16:46:42 -0800

    simple-shell

--- shellheader.h
+++ shellheader.h
@@ -11,6 +11,32 @@
 #include <stdbool.h>
 
 
+/**
+ * struct list_s - linked list of variables
+ * @value: value
+ * @next: pointer to next node
+ *
+ * Description: generic linked list struct for variables.
+**/
+typedef struct list_s
+{
+	char *value;
+	struct list_s *next;
+} list_s;
+
+/**
+ * struct built_s - linked list of builtins
+ * @name: name of builtin
+ * @p: pointer to function
+ *
+ * Description: struct for builtin functions.
+**/
+typedef struct built_s
+{
+	char *name;
+	int (*p)(void);
+} built_s;
+
 void exec_shell(char **args);
 char *read_line_shell(void);
 char **split_line(char *line);
